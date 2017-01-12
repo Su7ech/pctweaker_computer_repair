121193f2ae2102739efc98f18e471938aab50a03
Thomas Baroody <tpbaroody@gmail.com>
2017-01-11 19:10:24 -0800

    Modified folder structure

--- gulpfile.js
+++ gulpfile.js
@@ -10,13 +10,14 @@ let autoprefixerOptions = {
 };
 
 const SASS_INCLUDES = [
-    path.join(__dirname, '/node_modules/bootstrap-sass/assets/stylesheets/'),
+    path.join(__dirname, '/src/vendors/bootstrap/stylesheets/'),
     path.join(__dirname, '/node_modules/normalize-scss/fork-versions/default/')
 ];
 const SRC_FILES = {
     pug: path.join(__dirname, '/src/pug/'),
     sass: path.join(__dirname, '/src/scss/'),
-    scripts: path.join(__dirname, '/src/scripts/')
+    scripts: path.join(__dirname, '/src/scripts/'),
+    vendor: path.join(__dirname, '/src/vendors/')
 };
 const BUILD = {
     html: path.join(__dirname, '/src/build/'),
@@ -27,7 +28,7 @@ const SERVER = path.join(__dirname, '/src/build/');
 
 gulp.task('server', () => {
     browserSync.init({
-        server: BUILD.html,
+        server: SERVER,
         notify: false
     });
 });
@@ -53,11 +54,24 @@ gulp.task('pug', () => {
         .pipe(browserSync.stream());
 });
 
+gulp.task('concat', () => {
+    return gulp
+        .src([
+            SRC_FILES.vendor + 'jquery/jquery.js',
+            SRC_FILES.vendor + 'bootstrap/javascripts/bootstrap.js'
+        ])
+        .pipe($.sourcemaps.init())
+        .pipe($.concat('script.js'))
+        .pipe($.sourcemaps.write(BUILD.js))
+        .pipe(gulp.dest(BUILD.js));
+});
+
+
 gulp.task('watch', () => {
     gulp.watch(SRC_FILES.pug + '**/*.pug', ['pug']);
     gulp.watch(SRC_FILES.sass + '**/*.+(scss|sass)', ['sass']);
 });
 
 gulp.task('default', (cb) => {
-    runSequence(['pug', 'bundle', 'sass'], 'server', 'watch');
+    runSequence(['pug', 'concat', 'sass'], 'server', 'watch', cb);
 });
